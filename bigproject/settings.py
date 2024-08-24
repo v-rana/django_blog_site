bfb8a6b129da5f02fc4159df27833d7b3c32b629
v-rana <63035799+v-rana@users.noreply.github.com>
2024-08-24 16:15:23 +0530

    some minor changes in text

--- bigproject/settings.py
+++ bigproject/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'static/'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = "media/"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
